b804b98001b5af9e7319678592482337c0d31e80
elb-bme <79838492+elb-bme@users.noreply.github.com>
2023-05-26 15:00:32 +0200

    stylesheet updates

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QFile styleSheetFile("./SpyBot.qss");
+    styleSheetFile.open(QFile::ReadOnly);
+    QString stylesheet = QLatin1String(styleSheetFile.readAll());
+    a.setStyleSheet(stylesheet);
     MainWindow w;
     w.show();
     return a.exec();
